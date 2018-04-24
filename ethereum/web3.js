589badab3c92903b454690a63083041186e3ffea
Chirag Saini <chirag.saini@hotmail.com>
2018-04-25 00:48:19 +0530

    2nd commit

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,3 +1,18 @@
 import Web3 from 'web3';
-const web3 = new Web3(window.web3.currentProvider);
+//const web3 = new Web3(window.web3.currentProvider);
+let web3;
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined'){
+    // present in browser and metamask is running
+    web3 = new Web3(window.web3.currentProvider);
+}
+else { 
+    // we are on the server OR the user is not running metamask
+    //set up own provider
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/NIPz5KYjZxy3U9FpOGMj'
+    );
+
+    web3 = new Web3(provider);
+
+}
 export default web3;
